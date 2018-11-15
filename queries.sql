b6cced72f171f6aff1603e2edd074a85062beaca
Eckeau <b.novotny@bluewin.ch>
2018-11-15 21:35:43 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -12,4 +12,16 @@ ON f.KUNDE_ID_DIM = k.KUNDE_ID_DIM
 JOIN northwind_dwh.produkt_dim p
 ON f.PRODUKT_ID_DIM = p.PRODUKT_ID_DIM
 WHERE k.COMPANY = "Company A" AND
-	  p.PRODUKT_NAME = "Northwind Traders Coffee                ";
+	  p.PRODUKT_NAME = "Northwind Traders Coffee";
+      
+      
+SELECT ORDER_MONTH, r.STATE_PROVINCE, f.BRUTTO_ERLOES
+FROM northwind_dwh.verkauf_fact f
+JOIN northwind_dwh.region_dim r
+ON f.REGION_ID_DIM = r.REGION_ID_DIM
+JOIN northwind_dwh.produkt_dim p
+ON f.PRODUKT_ID_DIM = p.PRODUKT_ID_DIM
+JOIN northwind_dwh.zeit_dim t
+ON f.ZEIT_ID_DIM = t.ZEIT_ID_DIM
+WHERE p.PRODUKT_NAME = "Northwind Traders Chocolate" AND r.STATE_PROVINCE = "OR"
+GROUP BY t.ORDER_MONTH;
